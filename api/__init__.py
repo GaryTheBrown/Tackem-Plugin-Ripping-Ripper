74ad60290171d49a3a46d8fadb9c2f4315cdcdf4
Gary The Brown <GaryBrownSunderland@gmail.com>
2019-10-09 23:08:29 +0100

    new api update

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,13 @@
-'''API FOR PLUGIN'''
-def api(kwargs, apimode):
-    '''Ripper api'''
+'''PLUGIN API ROOT'''
+import cherrypy
+from api.system_base import APISystemBase
 
-    return "RIPPER API"
+@cherrypy.expose
+class API(APISystemBase):
+    '''PLUGIN API ROOT'''
+
+    def _cp_dispatch(self, vpath):
+        '''cp dispatcher overwrite'''
+        # action = vpath.pop(0)
+
+        return self
