bf53f663bdb1274f8619f87e5558709747b09ab2
Gary The Brown <GaryBrownSunderland@gmail.com>
2020-02-24 20:47:04 +0000

    updated config to new way

--- presets/__init__.py
+++ presets/__init__.py
@@ -2,15 +2,14 @@
 import os
 import json
 import glob
-from libs.config_option import ConfigOption
 
 _DIR = os.path.dirname(__file__)
 _VIDEO_PRESET_FILES = glob.glob(_DIR + "/video/*.json")
 _VIDEO_PRESET_FILES_DATA = [json.loads(str(open(x).read())) for x in _VIDEO_PRESET_FILES]
 
-def video_presets_config_options():
+def video_presets_config_options(obj):
     '''returns a list of 2 letter codes'''
-    return [ConfigOption(x['filename'], x['label']) for x in _VIDEO_PRESET_FILES_DATA]
+    return [obj(x['filename'], x['label']) for x in _VIDEO_PRESET_FILES_DATA]
 
 def get_video_preset_command(filename):
     '''returns the command from the video preset list'''
